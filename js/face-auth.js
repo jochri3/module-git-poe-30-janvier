b2e0de660a488b5d3431805087430e434c1e73ab
Christian Lisangola <jochri3@gmail.com>
2023-01-31 16:09:43 +0300

    Add google auth
    If account already exists, log the user, otherwise create an account and log the user

---
+++ js/face-auth.js
@@ -0,0 +1,17 @@
+export function handleFacebookAuth(clientId: string): void {
+  //     dfgj;klfgnssfsadnf
+  //     sdkfodpfl;msa;fmad
+  //     fsafjoi;sdalfdmsaf
+  //     sfjisal'kfmsadf
+  //     dsfjsa'ofsd
+  //     fodasp[ofjasd
+  //     [flasdf
+  //     ;ou
+  // dsfjsa'fdasfldsaf[p]iosd]a[
+  //     f'lads
+  //     '[f]podsa'
+  //     flsdp'fgkds
+  //     fg'kfap[]gik
+  //     f'dgk][dfsp;gkfs]
+  // ]]
+}
